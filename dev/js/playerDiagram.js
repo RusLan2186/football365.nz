2929d3513536b03cd5d584070671765ca94fe61a
Ruslan <4irus86@gmail.com>
2025-08-18 12:57:25 +0300

    adaptive: player  page

--- dev/js/playerDiagram.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.circle__block').forEach(block => {
-  const span = block.querySelector('span');
-  const value = parseInt(span.textContent); 
-  block.style.setProperty('--percent', value + '%');
-});
-
